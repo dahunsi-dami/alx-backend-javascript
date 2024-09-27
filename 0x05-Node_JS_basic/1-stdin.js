d1e03a5abbd2c5d616e3a0f7a8c3ff0b511e2fa1
dahunsi-dami <gbemigadare@gmail.com>
2024-09-27 01:29:17 +0000

    Make 1-stdin.js output closing msg when input's piped.

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -3,6 +3,9 @@ process.stdout.write('Welcome to Holberton School, what is your name?\n');
 process.stdin.on('data', (data) => {
   const name = data.toString();
   process.stdout.write(`Your name is: ${name}`);
+  if (!process.stdin.isTTY) {
+    process.stdout.write('This important software is now closing\n');
+  }
   process.exit();
 
   process.on('exit', () => {
