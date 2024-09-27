82c27fa50d479846af7268a1d14002fcc9c3f1a9
dahunsi-dami <gbemigadare@gmail.com>
2024-09-27 04:17:03 +0000

    Fix eslint errors.

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -3,16 +3,16 @@ const fs = require('fs');
 function countStudents(path) {
   try {
     const data = fs.readFileSync(path, 'utf8');
-    const lines = data.split('\n').filter(line => line.trim() !== '');
-    
+    const lines = data.split('\n').filter((line) => line.trim() !== '');
+
     if (lines.length <= 1) {
       throw new Error('Cannot load the database');
     }
 
-    const students = lines.slice(1).map(line => line.split(','));
+    const students = lines.slice(1).map((line) => line.split(','));
     const fields = {};
 
-    students.forEach(student => {
+    students.forEach((student) => {
       const field = student[3];
       if (!fields[field]) {
         fields[field] = [];
