6ebbedd3b97bd790c22a776a8fcb28b4c20a41a0
dahunsi-dami <gbemigadare@gmail.com>
2024-09-27 04:07:05 +0000

    Read a file sync w/ Node JS as required.

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+
+function countStudents(path) {
+  try {
+    const data = fs.readFileSync(path, 'utf8');
+    const lines = data.split('\n').filter(line => line.trim() !== '');
+    
+    if (lines.length <= 1) {
+      throw new Error('Cannot load the database');
+    }
+
+    const students = lines.slice(1).map(line => line.split(','));
+    const fields = {};
+
+    students.forEach(student => {
+      const field = student[3];
+      if (!fields[field]) {
+        fields[field] = [];
+      }
+      fields[field].push(student[0]);
+    });
+
+    console.log(`Number of students: ${students.length}`);
+    for (const [field, students] of Object.entries(fields)) {
+      console.log(`Number of students in ${field}: ${students.length}. List: ${students.join(', ')}`);
+    }
+  } catch (error) {
+    throw new Error('Cannot load the database');
+  }
+}
+
+module.exports = countStudents;
