30b56e552a4c5144fa7ec3500752322c2557382c
dahunsi-dami <gbemigadare@gmail.com>
2024-09-27 01:21:14 +0000

    Push code that passed test case 1-stdin.test.js.

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,12 +1,11 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('data', (data) => {
-  const name = data.toString().trim();
-  process.stdout.write(`Your name is: ${name}\n`);
-
-  if (!process.stdin.isTTY) {
-    console.log('This important software is now closing');
-  }
-
+  const name = data.toString();
+  process.stdout.write(`Your name is: ${name}`);
   process.exit();
+
+  process.on('exit', () => {
+    process.stdout.write('This important software is now closing\n');
+  });
 });
