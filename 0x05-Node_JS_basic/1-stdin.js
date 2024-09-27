7d4f746540d8347fbe352fe3844dd51b8358c295
dahunsi-dami <gbemigadare@gmail.com>
2024-09-27 01:47:11 +0000

    See if this passes red checks , sigh.

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,12 +1,16 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.on('data', (data) => {
-  const name = data.toString();
-  process.stdout.write(`Your name is: ${name}`);
-  if (!process.stdin.isTTY) {
-    process.stdout.write('This important software is now closing\n');
+process.stdin.on('readable', () => {
+  const readline = process.stdin.read();
+
+  if (readline) {
+    const name = readline.toString();
+    process.stdout.write(`Your name is: ${name}`);
+    if (!process.stdin.isTTY) {
+      process.stdout.write('This important software is now closing\n');
   }
   process.exit();
+}
 
   process.on('exit', () => {
     process.stdout.write('This important software is now closing\n');
