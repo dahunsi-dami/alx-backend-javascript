06278e2e3502039240e9e48d3efa13910a9fc974
dahunsi-dami <gbemigadare@gmail.com>
2024-10-21 22:32:47 +0100

    Add condition for when startString passed isn't a string.

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,7 +1,7 @@
 export default function cleanSet(set, startString) {
   const result = [];
 
-  if (startString === '') {
+  if (startString === '' || typeof startString !== 'string') {
     return '';
   }
 
