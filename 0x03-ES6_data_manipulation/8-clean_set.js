c688909887e1c96d2080298e87fd1b6810c7f39d
dahunsi-dami <gbemigadare@gmail.com>
2024-10-21 22:30:06 +0100

    Return empty str if startString is empty.

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,6 +1,10 @@
 export default function cleanSet(set, startString) {
   const result = [];
 
+  if (startString === '') {
+    return '';
+  }
+
   for (const value of set) {
     if (value.startsWith(startString)) {
       result.push(value.slice(startString.length));
