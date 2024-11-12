b43975cea061b5c59fa51b4f5aecf908689ba499
dahunsi-dami <gbemigadare@gmail.com>
2024-11-12 16:34:19 +0100

    Print teachers & write a class (task 3 and 4).

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -16,3 +16,60 @@ const teacher3: Teacher = {
 };
 
 console.log(teacher3);
+
+interface Directors extends Teacher {
+  numberOfReports: number;
+}
+
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+const printTeacher: printTeacherFunction = (firstName, lastName) => {
+  return `${firstName[0]}. ${lastName}`;
+};
+
+console.log(printTeacher("John", "Doe"));
+
+const director1: Directors = {
+  firstName: 'John',
+  fullTimeEmployee: true,
+  lastName: 'Doe',
+  location: 'London',
+  numberOfReports: 17,
+};
+
+console.log(director1);
+
+interface StudentConstructor {
+  new(firstName: string, lastName: string): StudentClass;
+}
+
+interface StudentClass {
+  firstName: string;
+  lastName: string;
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+class Student implements StudentClass {
+  firstName: string;
+  lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomework(): string {
+    return "Currently working";
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
+}
+
+const student1 = new Student("Jane", "Doe");
+console.log(student1.workOnHomework());
+console.log(student1.displayName());
